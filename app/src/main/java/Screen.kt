7d37dec081c7e87041a7c5f035accfcdbb26c24b
Mateo-A19 <aslan19070620@gmail.com>
2025-10-07 21:52:59 -0600

    Bandera de Italia con ConstraintLayout

--- app/src/main/java/Screen.kt
+++ app/src/main/java/Screen.kt
@@ -3,30 +3,26 @@ package com.example.unidad1
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
-fun BanderaJapon() {
+fun BanderaItalia() {
     ConstraintLayout(
         modifier = Modifier.fillMaxSize()
     ) {
-        val (fondo, sol) = createRefs()
+        val (verde, blanco, rojo) = createRefs()
 
         Box(
             modifier = Modifier
-                .background(Color.White)
-                .constrainAs(fondo) {
+                .background(Color(0xFF009246))
+                .constrainAs(verde) {
                     start.linkTo(parent.start)
                     top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
-                    end.linkTo(parent.end)
+                    end.linkTo(blanco.start)
                     width = androidx.constraintlayout.compose.Dimension.fillToConstraints
                     height = androidx.constraintlayout.compose.Dimension.fillToConstraints
                 }
@@ -34,14 +30,27 @@ fun BanderaJapon() {
 
         Box(
             modifier = Modifier
-                .size(180.dp)
-                .clip(CircleShape)
-                .background(Color(0xFFBC002D))
-                .constrainAs(sol) {
-                    start.linkTo(parent.start)
+                .background(Color.White)
+                .constrainAs(blanco) {
+                    start.linkTo(verde.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(rojo.start)
+                    width = androidx.constraintlayout.compose.Dimension.fillToConstraints
+                    height = androidx.constraintlayout.compose.Dimension.fillToConstraints
+                }
+        )
+
+        Box(
+            modifier = Modifier
+                .background(Color(0xFFCE2B37))
+                .constrainAs(rojo) {
+                    start.linkTo(blanco.end)
                     top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
                     end.linkTo(parent.end)
+                    width = androidx.constraintlayout.compose.Dimension.fillToConstraints
+                    height = androidx.constraintlayout.compose.Dimension.fillToConstraints
                 }
         )
     }
