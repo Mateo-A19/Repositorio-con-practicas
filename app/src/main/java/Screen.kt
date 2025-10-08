baecf0a229d70824ed8f2aa19c32ce0697d526d2
Mateo-A19 <aslan19070620@gmail.com>
2025-10-07 21:20:53 -0600

    Bandera de Japón

--- app/src/main/java/Screen.kt
+++ app/src/main/java/Screen.kt
@@ -1,50 +1,30 @@
 package com.example.unidad1
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun BanderaMexico() {
-    Row(modifier = Modifier.fillMaxSize()) {
-
+fun BanderaJapon() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
         Box(
             modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFF006847))
-        )
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color.White),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Image(
-                painter = painterResource(id = R.drawable.escudo),
-                contentDescription = "Escudo de México",
-                modifier = Modifier.size(150.dp)
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color(0xFFCE1126))
+                .size(180.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFBC002D))
         )
     }
 }
