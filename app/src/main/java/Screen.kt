f22d84ad9d29eea41f5f95d8f598f75ffa80f656
Mateo-A19 <aslan19070620@gmail.com>
2025-10-07 21:54:22 -0600

    Bandera de Chile con ConstraintLayout

--- app/src/main/java/Screen.kt
+++ app/src/main/java/Screen.kt
@@ -3,25 +3,30 @@ package com.example.unidad1
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
-fun BanderaItalia() {
+fun BanderaChile() {
     ConstraintLayout(
         modifier = Modifier.fillMaxSize()
     ) {
-        val (verde, blanco, rojo) = createRefs()
+        val (azul, blanco, rojo, estrella) = createRefs()
 
         Box(
             modifier = Modifier
-                .background(Color(0xFF009246))
-                .constrainAs(verde) {
+                .background(Color(0xFF0039A6))
+                .constrainAs(azul) {
                     start.linkTo(parent.start)
                     top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
+                    bottom.linkTo(rojo.top)
                     end.linkTo(blanco.start)
                     width = androidx.constraintlayout.compose.Dimension.fillToConstraints
                     height = androidx.constraintlayout.compose.Dimension.fillToConstraints
@@ -32,10 +37,10 @@ fun BanderaItalia() {
             modifier = Modifier
                 .background(Color.White)
                 .constrainAs(blanco) {
-                    start.linkTo(verde.end)
+                    start.linkTo(azul.end)
                     top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    end.linkTo(rojo.start)
+                    bottom.linkTo(rojo.top)
+                    end.linkTo(parent.end)
                     width = androidx.constraintlayout.compose.Dimension.fillToConstraints
                     height = androidx.constraintlayout.compose.Dimension.fillToConstraints
                 }
@@ -43,14 +48,29 @@ fun BanderaItalia() {
 
         Box(
             modifier = Modifier
-                .background(Color(0xFFCE2B37))
+                .background(Color(0xFFD52B1E))
                 .constrainAs(rojo) {
-                    start.linkTo(blanco.end)
+                    start.linkTo(parent.start)
                     top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
                     end.linkTo(parent.end)
                     width = androidx.constraintlayout.compose.Dimension.fillToConstraints
                     height = androidx.constraintlayout.compose.Dimension.fillToConstraints
+                    verticalBias = 1f
+                }
+        )
+
+        Icon(
+            imageVector = Icons.Default.Star,
+            contentDescription = "Estrella",
+            tint = Color.White,
+            modifier = Modifier
+                .size(60.dp)
+                .constrainAs(estrella) {
+                    start.linkTo(azul.start)
+                    top.linkTo(azul.top)
+                    bottom.linkTo(azul.bottom)
+                    end.linkTo(azul.end)
                 }
         )
     }
